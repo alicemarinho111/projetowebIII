3ed19d668285888cab98c5c668c18b593e024f16
Alice Marinho <mams3@discente.ifpe.edu.br>
2024-12-26 14:05:52 -0300

    conserto de errinhos bobos

--- middlewares/usuarioAuth.js
+++ middlewares/usuarioAuth.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 function auth (req, res, next){
     if (req.session.usuario){
         next(); 
@@ -8,14 +6,4 @@ function auth (req, res, next){
     }
 }
 
-=======
-function auth(req, res, next){
-    if(req.session.email){   
-    next();    
-    } else{
-    res.redirect("/usuarios/login");   
-    }  
-}
-    
->>>>>>> 062ea15592da8532691077109f168ca4bad070df
 module.exports = auth;
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,31 +3,13 @@ const routes = express.Router();
 const usuarioController = require("../controllers/usuarioController");
 const auth = require("../middlewares/usuarioAuth");
 
-
-<<<<<<< HEAD
-
-routes.get("/usuarios", usuarioController.listar);
-routes.post("/usuarios/", usuarioController.cadastrarPost);
-routes.get("/usuarios/cadastrar/:id?", usuarioController.cadastrarGet);
+routes.get("/usuarios", auth, usuarioController.listar);
+routes.post("/usuarios", auth, usuarioController.cadastrarPost);
+routes.get("/usuarios/cadastrar/:id?", auth, usuarioController.cadastrarGet);
 routes.get("/usuarios/remover/:id", auth, usuarioController.remover);
 routes.get("/usuarios/login", usuarioController.loginGet);
 routes.get("/usuarios/logout", usuarioController.logout);
 routes.post("/usuarios/login", usuarioController.loginPost);
 routes.get("/usuarios/:id", auth, usuarioController.detalhar);
 
-
-
 module.exports = routes;
-=======
-routes.get("/usuarios", auth, usuarioController.listar);
-routes.post("/usuarios", auth, usuarioController.cadastrarPost);
-routes.get("/usuarios/cadastrar/:id?", auth, usuarioController.cadastrarGet);
-routes.get("/usuarios/remover/:id",auth, usuarioController.remover);
-routes.get("/usuarios/login", usuarioController.loginGet);
-routes.get("/usuarios/logout", usuarioController.logout);
-routes.post("/usuarios/login", usuarioController.loginPost);
-routes.get("/usuarios/:id", usuarioController.detalhar);
-
-module.exports = routes;
-
->>>>>>> 062ea15592da8532691077109f168ca4bad070df
